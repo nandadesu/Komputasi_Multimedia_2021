68710a0a7d497012a91fa793f1b690d20c60af2d
nandadesu <43083923+nandadesu@users.noreply.github.com>
2021-02-19 20:34:51 +0700

    commit

---
+++ Minggu-01/Hello World/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    public string myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("myText");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+        GUILayout.Label(myText);
+        GUILayout.EndArea();
+    }
+}
